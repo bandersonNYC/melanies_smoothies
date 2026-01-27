44970cb9e983183e439426a5deeb3cbfdc070c81
banderson-snow <sgjzfr8xjq@privaterelay.appleid.com>
2026-01-27 13:58:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,10 +47,9 @@ if ingredients_list:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
-
-
+#st.text(smoothiefroot_response)
 #st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
   
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
